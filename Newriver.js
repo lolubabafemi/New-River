aadceb3461cf4750cc629636bf2f8a2c419ae155
generaldraco <od.babafemi@gmail.com>
2024-04-08 16:15:51 +0100

    edit

--- Newriver.js
+++ Newriver.js
@@ -3,7 +3,9 @@
 
 var visitor = prompt('what is your name?')
 var greeting = 'Welcome ' + visitor;
+greeting += ". This is a 1 page website by Babafemi Omololu"
+greeting += ". A front-end Developer"
 alert(greeting);
 
-var message = "This is a 1 page website by Babafemi Omololu";
-alert(message);
+// var message = "This is a 1 page website by Babafemi Omololu";
+// alert(message);
